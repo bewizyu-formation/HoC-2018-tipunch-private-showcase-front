406760e03e42529a98a413d331bb9884a982c3b8
Julien Laforge <julien.laforge@gmx.fr>
2018-11-13 18:10:33 +0100

    Tests json

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewChecked {
 
-  constructor() { }
+  title = 'Private ShowCase';
+  datas : Object;
+  homeDescription :string;
+  homeIntroduction :string;
+  
+  constructor(private http: HttpClient) {
+
+    
+    this.datas = this.http.get('http://localhost:4200/assets/jsondatas.json')
+      .subscribe(data => {
+        this.datas = data;
+        
+        this.homeDescription = this.datas['home'].description;
+        this.homeIntroduction = this.datas['home'].introduction;
+      });
+      
+  }
 
   ngOnInit() {
+   
+
+  }
+
+
+  ngAfterViewChecked() {
+   // console.table(this.datas);
   }
 
 }
